24ae0812e356cecfa24572215d74e17ddbe61829
donikast <80174917+donikast@users.noreply.github.com>
2025-03-29 22:13:24 +0200

    Update Main.java

--- src/org/graphics/Main.java
+++ src/org/graphics/Main.java
@@ -4,7 +4,7 @@ import org.graphics.renders.Window;
 
 public class Main {
     public static void main(String[] args) {
-        Window window = new Window(800, 800, "OpenGL with Shaders");
+        Window window = new Window(800, 800, "Independent work #1");
         window.run();
     }
 }
